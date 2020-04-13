3c52fe4c359d323576cf8cb58a3b736025bb75f3
Dylan Nguyen <dylannguyen1209@gmail.com>
2020-04-13 22:50:27 +0100

    Time Series

---
+++ Time Series in R/1.Exploratory time series data analysis.R
@@ -0,0 +1,13 @@
+library(datasets)
+Nile <-as.vector(Nile)
+# Print the Nile dataset
+print(Nile)
+
+# List the number of observations in the Nile dataset
+
+length(Nile)
+# Display the first 10 elements of the Nile dataset
+head(Nile, n = 10)
+
+# Display the last 12 elements of the Nile dataset
+tail(Nile,n= 12)
