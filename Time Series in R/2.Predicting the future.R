39ebb6be08b23729186f90bdcdb8d0624efb9295
Dylan Nguyen <dylannguyen1209@gmail.com>
2020-04-21 14:29:43 +0100

    White noise

--- Time Series in R/2.Predicting the future.R
+++ Time Series in R/2.Predicting the future.R
@@ -13,3 +13,39 @@ linear_growth <- log(rapid_growth)
 
 # Plot linear_growth using ts.plot()
 ts.plot(linear_growth)
+
+#
+## Removing trends in level by differencing
+#
+
+# Generate the first difference of z
+dz <- diff(z)
+
+# Plot dz
+ts.plot(dz)
+
+# View the length of z and dz, respectively
+length(z)
+length(dz)
+
+#
+##Removing seasonal trends with seasonal differencing
+#
+# Generate a diff of x with lag = 4. Save this to dx
+dx <- diff(x,lag = 4)
+
+# Plot dx
+ts.plot(dx)  
+
+# View the length of x and dx, respectively 
+
+length(x)
+length(dx)
+
+#
+## White noise model
+#
+## Simulate 50 observations from the WN model
+WN_1 <-arima.sim(model = list(order = c(0,0,0)), n =50)
+head(WN_1)
+ts.plot(WN_1)
