93e8e1555ffdcb1ddbac85e013fdfcf339886b55
Dylan Nguyen <dylannguyen1209@gmail.com>
2020-05-11 22:46:42 +0100

    more visualisation

--- Time Series in R/Case Studies Manipulating Time Series Data in R/2. Weather Data.R
+++ Time Series in R/Case Studies Manipulating Time Series Data in R/2. Weather Data.R
@@ -1,3 +1,14 @@
 #
 ## Merging time series data by row using rbind()
 #
+# View the structure of each object
+str(temps_1)
+str(temps_2)
+
+# View the first and last rows of temps_1
+head(temps_1)
+tail(temps_1)
+
+# View the first and last rows of temps_2
+head(temps_2)
+tail(temps_2)
