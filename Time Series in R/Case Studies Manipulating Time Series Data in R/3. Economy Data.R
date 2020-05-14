056779122395f58bb516dad7b30dd7ea7a5c6af1
Dylan Nguyen <dylannguyen1209@gmail.com>
2020-05-14 23:37:23 +0100

    discrete rolling sum

--- Time Series in R/Case Studies Manipulating Time Series Data in R/3. Economy Data.R
+++ Time Series in R/Case Studies Manipulating Time Series Data in R/3. Economy Data.R
@@ -74,3 +74,71 @@ unemployment <- na.approx(unemployment)
 # Plot new unemployment data
 plot.zoo(unemployment, plot.type = "single", lty = lty)
 legend("topright", lty = lty, legend = labels, bg = "white")
+
+#
+## Lagging and unemployment
+#
+# Create a one month lag of US unemployment
+us_monthlag <- lag(unemployment$us, k = 1)
+
+# Create a one year lag of US unemployment
+us_yearlag <- lag(unemployment$us, k =12)
+
+# Merge original unemployment data with your new lags 
+unemployment_lags <- merge(unemployment, us_monthlag, us_yearlag)
+
+# View the first 15 rows of unemployment_lags
+head(unemployment_lags, n =15)
+
+#
+## Differencing unemployment
+#
+# Generate monthly difference in unemployment
+unemployment$us_monthlydiff <- diff(unemployment$us, lag = 1, differences = 1)
+
+# Generate yearly difference in unemployment
+unemployment$us_yearlydiff <- diff(unemployment$us, lag = 12, differences = 1)
+
+
+# Plot US unemployment and annual difference
+par(mfrow = c(2,1))
+plot.xts(unemployment$us)
+plot.xts(unemployment$us_yearlydiff, type = "h") # barplot
+
+#
+## Add a discrete rolling sum to GDP data
+#
+gdp <- readRDS("us_gdp.RData")
+# Convert GDP date column to time object
+gdp$date <- as.yearqtr(gdp$date)
+
+# Convert GDP data to xts
+gdp <- as.xts(gdp[, -1], order.by = gdp$date)
+colnames(gdp) <- c("gdp")
+
+## Replace missing data - II using linear approximation
+
+# Fill NAs in gdp_xts using linear approximation
+gdp_approx <- na.approx(gdp_xts) 
+
+# Plot your new xts object
+plot.xts(gdp_approx, major.format = "%Y")
+
+# Query for GDP in 1993 in gdp_approx
+gdp_approx["1993"]
+
+
+# Add a quarterly difference in gdp
+gdp$quarterly_diff <- diff(gdp$gdp, lag = 1, differences = 1)
+
+# Split gdp$quarterly_diff into years
+gdpchange_years <- split(gdp$quarterly_diff, f = "years")
+
+# Use lapply to calculate the cumsum each year
+gdpchange_ytd <- lapply(gdpchange_years, FUN = cumsum)
+
+# Use do.call to rbind the results
+gdpchange_xts <- do.call(rbind, gdpchange_ytd)
+
+# Plot cumulative year-to-date change in GDP
+plot.xts(gdpchange_xts, type = "h")
