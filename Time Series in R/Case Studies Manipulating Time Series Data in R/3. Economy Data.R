aa0270ef6fa30b885a77ebbef3cbaad10fa89c36
Dylan Nguyen <dylannguyen1209@gmail.com>
2020-05-15 02:10:02 +0100

    Finish chapter 3

--- Time Series in R/Case Studies Manipulating Time Series Data in R/3. Economy Data.R
+++ Time Series in R/Case Studies Manipulating Time Series Data in R/3. Economy Data.R
@@ -142,3 +142,32 @@ gdpchange_xts <- do.call(rbind, gdpchange_ytd)
 
 # Plot cumulative year-to-date change in GDP
 plot.xts(gdpchange_xts, type = "h")
+
+#
+##Add a continuous rolling average to unemployment data
+#
+# Use rollapply to calculate the rolling yearly average US unemployment
+unemployment$year_avg <- rollapply(unemployment$us, width =12 , FUN = "mean")
+
+lty = c(2,1)
+lwd = c(1,2)
+# Plot all columns of US unemployment data
+plot.zoo(unemployment[, c("us", "year_avg")], plot.type = "single", lty = lty, lwd = lwd)
+
+#
+## Manipulating MA unemployment data
+#
+# Add a one-year lag of MA unemployment
+unemployment$ma_yearlag <- lag(unemployment$ma, k = 12)
+
+# Add a six-month difference of MA unemployment
+unemployment$ma_sixmonthdiff <- diff(unemployment$ma, lag = 6, differences = 1)
+
+# Add a six-month rolling average of MA unemployment
+unemployment$ma_sixmonthavg <- rollapply(unemployment$ma, width = 6, FUN = mean)
+
+# Add a yearly rolling maximum of MA unemployment
+unemployment$ma_yearmax <- rollapply(unemployment$ma, width = 12, FUN = max)
+
+# View the last year of unemployment data
+tail(unemployment, n = 12)
